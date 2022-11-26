2fcdb4c71ccc9e23d91b23aedb8c805ac64bc75e
reyno405 <reyno405@gmail.com>
2022-11-26 14:54:34 -0500

    this has a readme and the code

--- server.js
+++ server.js
@@ -14,7 +14,6 @@ app.use(require('./routes'));
 
 // Connect mongoose
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/social-network-api', {
-  useFindAndModify: false,
   useNewUrlParser: true,
   useUnifiedTopology: true
 });
